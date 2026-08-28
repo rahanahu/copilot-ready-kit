92ce08af6d388d4cb68d6b51a6dda60c4edfddb1
rahanahu <rahanahu@gmail.com>
2026-08-29 00:41:33 +0900

    feat: add theme synchronization view model

---
+++ examples/angular-reactive-patterns/theme.vm.ts
@@ -0,0 +1,13 @@
+import { effect, signal } from '@angular/core';
+
+export class ThemeViewModel {
+  readonly theme = signal<'light' | 'dark'>('light');
+
+  constructor() {
+    effect(() => {
+      const theme = this.theme();
+      localStorage.setItem('theme', theme);
+      document.documentElement.dataset['theme'] = theme;
+    });
+  }
+}
